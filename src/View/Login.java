452115d61683816eeb5783693f4487fd9f18b12e
George <george_bergson@hotmail.com>
2023-09-02 13:05:49 -0400

    Pagina de Login

--- src/View/Login.java
+++ src/View/Login.java
@@ -26,18 +26,39 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        txfUsuario = new javax.swing.JTextField();
+        txfSenha = new javax.swing.JPasswordField();
+        lblUsuario = new javax.swing.JLabel();
+        lblSenha = new javax.swing.JLabel();
+        lblLogin = new javax.swing.JLabel();
+        painelFundo = new javax.swing.JLabel();
+        backgroundLogin = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(txfUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 500, 320, 50));
+        getContentPane().add(txfSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 600, 320, 50));
+
+        lblUsuario.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblUsuario.setForeground(new java.awt.Color(255, 255, 255));
+        lblUsuario.setText("Usuario");
+        getContentPane().add(lblUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 470, -1, -1));
+
+        lblSenha.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblSenha.setForeground(new java.awt.Color(255, 255, 255));
+        lblSenha.setText("Senha");
+        getContentPane().add(lblSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 570, -1, -1));
+
+        lblLogin.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
+        lblLogin.setForeground(new java.awt.Color(255, 255, 255));
+        lblLogin.setText("Login");
+        getContentPane().add(lblLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 330, -1, -1));
+
+        painelFundo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Images/painel-login.png"))); // NOI18N
+        getContentPane().add(painelFundo, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 300, -1, -1));
+
+        backgroundLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Images/Logo.jpg"))); // NOI18N
+        getContentPane().add(backgroundLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -78,5 +99,12 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel backgroundLogin;
+    private javax.swing.JLabel lblLogin;
+    private javax.swing.JLabel lblSenha;
+    private javax.swing.JLabel lblUsuario;
+    private javax.swing.JLabel painelFundo;
+    private javax.swing.JPasswordField txfSenha;
+    private javax.swing.JTextField txfUsuario;
     // End of variables declaration//GEN-END:variables
 }
