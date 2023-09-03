1400f41bb43c0a77f95805160597a85cce8b2e96
George <george_bergson@hotmail.com>
2023-09-03 10:37:46 -0400

    ajuste nas classes model com Herança

---
+++ src/Model/PessoaModel.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author user
+ */
+abstract public class PessoaModel {
+    protected int id;
+    protected String nome;
+    protected char sexo;
+    protected Date data_nascimento;
+    protected String telefone;
+    protected String email;
+    protected String rg;
+
+    public PessoaModel(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public PessoaModel(int id, String nome, char sexo, Date data_nascimento, String telefone, String email, String rg) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.data_nascimento = data_nascimento;
+        this.telefone = telefone;
+        this.email = email;
+        this.rg = rg;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    
+    
+}
--- src/Model/UsuarioModel.java
+++ src/Model/UsuarioModel.java
@@ -10,49 +10,18 @@ import java.util.Date;
  *
  * @author user
  */
-public class UsuarioModel {
-    private int id;
-    private String nome;
+public class UsuarioModel extends PessoaModel {
     private String senha;
     private String nivel_acesso;
-    private char sexo;
-    private Date data_nascimento;
-    private String telefone;
-    private String email;
-    private String rg;
 
-    public UsuarioModel(int id, String nome, String senha, String nivel_acesso, char sexo, Date data_nascimento, String telefone, String email, String rg) {
-        this.id = id;
-        this.nome = nome;
+    public UsuarioModel(int id, String nome) {
+        super(id, nome);
+    }
+
+    public UsuarioModel(String senha, String nivel_acesso, int id, String nome, char sexo, Date data_nascimento, String telefone, String email, String rg) {
+        super(id, nome, sexo, data_nascimento, telefone, email, rg);
         this.senha = senha;
         this.nivel_acesso = nivel_acesso;
-        this.sexo = sexo;
-        this.data_nascimento = data_nascimento;
-        this.telefone = telefone;
-        this.email = email;
-        this.rg = rg;
-    }
-
-    public UsuarioModel(int id, String nome, String senha) {
-        this.id = id;
-        this.nome = nome;
-        this.senha = senha;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getSenha() {
@@ -70,46 +39,11 @@ public class UsuarioModel {
     public void setNivel_acesso(String nivel_acesso) {
         this.nivel_acesso = nivel_acesso;
     }
+    
+    
 
-    public char getSexo() {
-        return sexo;
-    }
 
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
-
-    public Date getData_nascimento() {
-        return data_nascimento;
-    }
-
-    public void setData_nascimento(Date data_nascimento) {
-        this.data_nascimento = data_nascimento;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
+   
     
     
 }
